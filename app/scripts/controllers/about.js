4f0601b5622f5a9d289f185f9aa266575b9d6f3f
Obipolar <shaohuixu13@gmail.com>
2014-08-25 22:24:21 -0400

    default angular app generated

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name prismSiteApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the prismSiteApp
+ */
+angular.module('prismSiteApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
